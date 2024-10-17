8627f8bc175a1e49b4831c08041e3271c22938fd
Sariah Adams <sariah.adams@bison.howard.edu>
2024-10-17 00:58:33 +0000

    Project finished

---
+++ shell1.c
@@ -0,0 +1,107 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <signal.h>
+
+#define MAX_COMMAND_LINE_LEN 1024
+#define MAX_COMMAND_LINE_ARGS 128
+
+char prompt[] = "> ";
+char delimiters[] = " \t\r\n";
+extern char **environ;
+
+
+
+int main() {
+    // Stores the string typed into the command line.
+    char command_line[MAX_COMMAND_LINE_LEN];
+    char cmd_bak[MAX_COMMAND_LINE_LEN];
+  
+    // Stores the tokenized command line input.
+    char *arguments[MAX_COMMAND_LINE_ARGS];
+    	
+    while (true) {
+      
+        do{ 
+            // Print the shell prompt.
+            printf("%s", prompt);
+            fflush(stdout);
+
+            // Read input from stdin and store it in command_line. If there's an
+            // error, exit immediately. (If you want to learn more about this line,
+            // you can Google "man fgets")
+        
+            if ((fgets(command_line, MAX_COMMAND_LINE_LEN, stdin) == NULL) && ferror(stdin)) {
+                fprintf(stderr, "fgets error");
+                exit(0);
+            }
+ 
+        }while(command_line[0] == 0x0A);  // while just ENTER pressed
+
+      
+        // If the user input was EOF (ctrl+d), exit the shell.
+        if (feof(stdin)) {
+            printf("\n");
+            fflush(stdout);
+            fflush(stderr);
+            return 0;
+        }
+
+        // TODO:
+        // 
+        
+			  // 0. Modify the prompt to print the current working directory
+        void printDir()
+        {
+          char cwd[1024];
+          getcwd(cwd, sizeof(cwd));
+          printf("\nDir: %s", cwd);
+        }
+			
+        // 1. Tokenize the command line input (split it on whitespace)
+        char *token = strtok(command_line, delimiters); //First token
+        int i = 0;
+
+        while (token != NULL && i < MAX_COMMAND_LINE_ARGS - 1){
+          arguements[i] = token; //Stores each token (arguement)
+          i++;
+          token = strtok(NULL, delimiters); //Generates / gets the next token
+        }   
+        arguements[i] = NULL // Nu;;-terminate the arguement list
+
+        //Prints the tokens for testing
+        for (int j = 0; j < i; j++){
+          printf("Argument[%d]: %s\n", j, arguments[j]);
+    
+        }   
+
+        return 0;
+        // 2. Implement Built-In Commands
+      
+    
+        // 3. Create a child process which will execute the command line input
+
+  
+        // 4. The parent process should wait for the child to complete unless its a background process
+      
+      
+        // Hints (put these into Google):
+        // man fork
+        // man execvp
+        // man wait
+        // man strtok
+        // man environ
+        // man signals
+        
+        // Extra Credit
+        // man dup2
+        // man open
+        // man pipes
+    }
+    // This should never be reached.
+    return -1;
+}
